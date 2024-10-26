7327f646fae56d3ee4ca17314ec9b4b22b074bbf
Ram-J1967 <malabananramjhake@gmail.com>
2024-10-26 21:36:16 +0800

    Add files via upload

---
+++ GradingSystemNamin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradingsystemnamin;
+
+/**
+ *
+ * @author CC6828
+ */
+public class GradingSystemNamin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
